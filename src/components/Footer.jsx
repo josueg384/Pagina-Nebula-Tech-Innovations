4bc273c30d9abe451ea55d2e6e9e851f2b13ee4a
Kevin Llamas <kevin.koll3070@gmail.com>
2024-06-08 21:31:51 -0700

    kevin secciones

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import ".././css/header.css";
+import logo from '.././assets/logo-nebula.webp'; // Logo
+
+export function Footer() {
+  return (
+   <footer>
+     <div>------------------------------------Footer------------------------------------------</div>
+      <div>Blue</div>
+      <div>Black</div>
+   </footer>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import ReactDOM from "react-dom/client";
 //COMPONENTES
 import { Header } from "./components/Header.jsx";
 import { Sec1_Inicio } from "./components/Sec1_Inicio.jsx";
-
+import { Sec5_cliente } from "./components/Sec5_clientes.jsx";
+import { Sec6_reportes } from "./components/Sec6_reportes.jsx";
+import { Sec7_nuestros } from "./components/Sec7_nuestros.jsx";
+import { Footer } from "./components/Footer.jsx";
 //ESTILOS
 import "./css/normalize.css";
 import "./css/styles.css";
@@ -16,7 +19,11 @@ root.render(
     <Header />
     <main>
       <Sec1_Inicio />
+      <Sec5_cliente />
+      <Sec6_reportes/>
+      <Sec7_nuestros/>
     </main>
+    <Footer />
     
   </>
 );
